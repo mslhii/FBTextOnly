c40c4616d642ee0dcbbf79f2f67507e2de0ccbbd
Michael Hii <mslhii@yahoo.com>
2017-07-02 23:15:47 -0700

    Comment

--- src/com/kritikalerror/fbtextonly/MainActivity.java
+++ src/com/kritikalerror/fbtextonly/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends FragmentActivity implements
 
 			@Override
 			public void onPageSelected(int position) {
-				// On changing the page
+				// On changing the page,
 				// Make the respected tab selected
 				actionBar.setSelectedNavigationItem(position);
 			}
