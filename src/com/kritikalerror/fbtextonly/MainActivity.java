b214b0aabc084af5f8681e0bd7ecec9aacca3b7f
Michael Hii <mslhii@yahoo.com>
2017-12-11 01:04:33 -0800

    Comment

--- src/com/kritikalerror/fbtextonly/MainActivity.java
+++ src/com/kritikalerror/fbtextonly/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends FragmentActivity implements
 
 	@Override
 	public void onTabSelected(Tab tab, FragmentTransaction ft) {
-		// onTabSelected menu for
+		// onTabSelected menu for u
 		// Show respected fragment view to user
 		viewPager.setCurrentItem(tab.getPosition());
 	}
