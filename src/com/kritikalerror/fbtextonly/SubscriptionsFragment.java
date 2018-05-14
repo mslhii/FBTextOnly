5d48a3e5676c98df400d124012448b0c81284c01
Michael Hii <mslhii@yahoo.com>
2018-05-13 21:19:12 -0700

    Comment

--- src/com/kritikalerror/fbtextonly/SubscriptionsFragment.java
+++ src/com/kritikalerror/fbtextonly/SubscriptionsFragment.java
@@ -13,7 +13,7 @@ public class SubscriptionsFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		// Display the views to th
+		// Display the views to the
 		View rootView = inflater.inflate(R.layout.fragment_subscriptions, container, false);
 		
 		return rootView;
