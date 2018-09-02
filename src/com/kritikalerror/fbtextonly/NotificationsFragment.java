7f639333c5814057f3b4d77a50bc2c1e66d0a262
Michael Hii <mslhii@yahoo.com>
2018-09-01 23:49:41 -0500

    Comment

--- src/com/kritikalerror/fbtextonly/NotificationsFragment.java
+++ src/com/kritikalerror/fbtextonly/NotificationsFragment.java
@@ -13,7 +13,7 @@ public class NotificationsFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		//Display the view to the use
+		//Display the view to the user
 		View rootView = inflater.inflate(R.layout.fragment_notifications, container, false);
 		
 		return rootView;
