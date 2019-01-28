78c76c5d5c3a4e5e235e611c86de645eb4942905
Michael Hii <mslhii@yahoo.com>
2019-01-28 01:46:19 -0800

    Comment

--- src/com/kritikalerror/fbtextonly/MessagesFragment.java
+++ src/com/kritikalerror/fbtextonly/MessagesFragment.java
@@ -13,7 +13,7 @@ public class MessagesFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		//Display the view t
+		//Display the view to
 		View rootView = inflater.inflate(R.layout.fragment_messages, container, false);
 		
 		return rootView;
