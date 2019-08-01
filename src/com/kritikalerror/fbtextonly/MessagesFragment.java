3b8a8557b4819e00db50c2462f7babcdef75676c
Michael Hii <mslhii@yahoo.com>
2019-07-31 23:32:29 -0700

    Comment

--- src/com/kritikalerror/fbtextonly/MessagesFragment.java
+++ src/com/kritikalerror/fbtextonly/MessagesFragment.java
@@ -13,7 +13,7 @@ public class MessagesFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		//Display the view to the users so that they can see the view
+		//Display the view to the users so that they can see the views
 		View rootView = inflater.inflate(R.layout.fragment_messages, container, false);
 		
 		return rootView;
