3d095e373f2a2c0c2eaf666777a9631df897c263
Michael Hii <mslhii@yahoo.com>
2019-08-28 23:53:07 -0700

    Comment

--- src/com/kritikalerror/fbtextonly/LoginActivity.java
+++ src/com/kritikalerror/fbtextonly/LoginActivity.java
@@ -21,7 +21,7 @@ public class LoginActivity extends Activity {
 
 		// Login code, user must have an FB account. We need a shortcode
 
-		// Walk user through the login
+		// Walk user through the login p
 
 		// This should be the first activity
 	}
