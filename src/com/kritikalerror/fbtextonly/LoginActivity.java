9c8588e00337eabeb3ed91c8de310d237c793e04
Michael Hii <mslhii@yahoo.com>
2019-10-12 22:46:16 -0700

    Comment

--- src/com/kritikalerror/fbtextonly/LoginActivity.java
+++ src/com/kritikalerror/fbtextonly/LoginActivity.java
@@ -19,7 +19,7 @@ public class LoginActivity extends Activity {
 		ActionBar ab = getActionBar();
 		ab.hide();
 
-		// Login code, user must have an FB account. We need a shortcode to recei
+		// Login code, user must have an FB account. We need a shortcode to receiv
 
 		// Walk user through the login page
 
