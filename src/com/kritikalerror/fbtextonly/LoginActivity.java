ec2a9b0d94e33cc4d348599b4e9c6ec7237f4e0a
Michael Hii <mslhii@yahoo.com>
2021-02-13 16:15:06 -0800

    Comment

--- src/com/kritikalerror/fbtextonly/LoginActivity.java
+++ src/com/kritikalerror/fbtextonly/LoginActivity.java
@@ -23,7 +23,7 @@ public class LoginActivity extends Activity {
 		// display the view to the users so that the user can see the view as it should be seen.
 		//
 
-		// Walk user through the login page so t
+		// Walk user through the login page so th
 
 		// This should be the first activity
 	}
